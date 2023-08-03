d6360d7c9cc21143727faefd445b36fe233a6c91
MathieuNlp <mathieunalpon@gmail.com>
2023-08-03 21:43:59 +0200

    [+] Finished saving and loading functions

--- lora.py
+++ lora.py
@@ -93,6 +93,7 @@ class LoRA_sam(nn.Module):
 
 
     def reset_parameters(self):
+        # Initalisation like in the paper
         for w_A in self.A_weights:
             nn.init.kaiming_uniform_(w_A.weight, a=np.sqrt(5))
         for w_B in self.B_weights:
@@ -104,7 +105,7 @@ class LoRA_sam(nn.Module):
         num_layer = len(self.A_weights)
         # sufix 03:d -> allows to have a name 1 instead of 001
         a_tensors = {f"w_a_{i:03d}": self.A_weights[i].weight for i in range(num_layer)}
-        b_tensors = {f"w_a_{i:03d}": self.B_weights[i].weight for i in range(num_layer)}
+        b_tensors = {f"w_b_{i:03d}": self.B_weights[i].weight for i in range(num_layer)}
 
         dim_in = self.lora_vit.head.in_features
         dim_out = self.lora_vit.head.out_features
@@ -116,15 +117,49 @@ class LoRA_sam(nn.Module):
 
 
     def load_lora_parameters(self, filename):
-        
+        # load lora and fc parameters
+        with safe_open(filename, framework="pt") as f:
+            for i, w_A_linear in enumerate(self.A_weights):
+                saved_key = f"w_a_{i:03d}"
+                saved_tensor = f.get_tensor(saved_key)
+                w_A_linear.weight = nn.Parameter(saved_tensor)
 
-        return None
+            for i, w_B_linear in enumerate(self.B_weights):
+                saved_key = f"w_b_{i:03d}"
+                saved_tensor = f.get_tensor(saved_key)
+                w_B_linear.weight = nn.Parameter(saved_tensor)
 
+            dim_in = self.lora_vit.head.in_features
+            dim_out = self.lora_vit.head.out_features
+            saved_key =  f"fc_{dim_in}in_{dim_out}out"
+
+            try:
+                saved_tensors = f.get_tensor(saved_key)
+                self.lora_vit.weight = nn.Parameter(saved_tensor)
+            except ValueError:
+                print("This fc weight don't fit")
 
 
     def load_fc_parameters(self, filename):
+        
+        with safe_open(filename, framework="pt") as f:
 
-        return None
+            dim_in = self.lora_vit.head.in_features
+            dim_out = self.lora_vit.head.out_features
+            saved_key =  f"fc_{dim_in}in_{dim_out}out"
+
+            try:
+                saved_tensor = f.get_tensor(saved_key)
+                self.lora_vit.head.weight = nn.Parameter(saved_tensor)
+            except ValueError:
+                print("This fc don't fit")
+
+            
+if __name__ == "__main__":
+    sam = build_sam_vit_b(checkpoint="sam_vit_b_01ec64.pth")
+    sam_lora = LoRA_sam(sam,4)
+    sam_lora.sam.image_encoder(torch.rand(size=(1,3,512,512)))
+
 
 
 
