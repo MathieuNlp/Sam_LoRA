f3af132865ee7be586283e78bed5efb45e053904
Mathieu <mathieu.nalpon@student-cs.fr>
2023-09-10 14:41:53 +0200

    colab update

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ for epoch in range(num_epochs):
       ground_truth_masks = batch_inputs[0]["ground_truth_mask"].float().to(device)
       ground_truth_masks = ground_truth_masks[None, None, :, :]
       ground_truth_masks = torch.permute(ground_truth_masks, (0, 1, 3, 2))
+      print(ground_truth_masks.shape, gt_outputs_mask.shape)
       loss = seg_loss(gt_outputs_mask, ground_truth_masks)
 
       # backward pass (compute gradients of parameters w.r.t. loss)
