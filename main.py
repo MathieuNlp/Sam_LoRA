875001c9ceb74e89bbd919c54debc5b6ea5872c8
Mathieu <mathieu.nalpon@student-cs.fr>
2023-09-10 11:19:15 +0200

    edit output in colab

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ for epoch in range(num_epochs):
       # compute loss
       gt_outputs_mask = outputs[0]["masks"]
       #predicted_masks = outputs.masks.squeeze(1)
-      ground_truth_masks = batch_inputs["ground_truth_mask"].float() 
+      ground_truth_masks = batch_inputs[0]["ground_truth_mask"].float() 
 
       loss = seg_loss(gt_outputs_mask, ground_truth_masks)
 
