6502dcd8b55a210c79efa630ea9fb019c6accf5a
Mathieu <mathieu.nalpon@student-cs.fr>
2023-09-10 15:06:42 +0200

    colab update

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ dataset = DatasetSegmentation(dataset_path, processor)
 train_dataloader = DataLoader(dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
 optimizer = Adam(sam_lora.lora_vit.parameters(), lr=1e-5, weight_decay=0)
 seg_loss = monai.losses.DiceCELoss(sigmoid=True, squared_pred=True, reduction='mean')
-
+seg_loss.requires_grad = True
 num_epochs = 10
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -53,7 +53,7 @@ for epoch in range(num_epochs):
 
       # backward pass (compute gradients of parameters w.r.t. loss)
       optimizer.zero_grad()
-      loss.requires_grad = True
+      
       loss.backward()
 
       # optimize
