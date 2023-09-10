49204f14714b2c0190f8eb3d0a043beee3f42f8d
Mathieu <mathieu.nalpon@student-cs.fr>
2023-09-10 13:23:50 +0200

    edit output in colab

--- main.py
+++ main.py
@@ -44,9 +44,10 @@ for epoch in range(num_epochs):
 
       # compute loss
       gt_outputs_mask = outputs[0]["masks"]
-      print(gt_outputs_mask.shape, ground_truth_masks.shape)
+
       #predicted_masks = outputs.masks.squeeze(1)
       ground_truth_masks = batch_inputs[0]["ground_truth_mask"].float() 
+      print(gt_outputs_mask.shape, ground_truth_masks.shape)
       loss = seg_loss(gt_outputs_mask, ground_truth_masks)
 
       # backward pass (compute gradients of parameters w.r.t. loss)
