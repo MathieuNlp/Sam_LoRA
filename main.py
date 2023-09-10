82f3a2133474aaf15c58d0704720f0df1a0a33cf
Mathieu <mathieu.nalpon@student-cs.fr>
2023-09-10 14:06:48 +0200

    edit output in colab

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ for epoch in range(num_epochs):
 
       #predicted_masks = outputs.masks.squeeze(1)
       ground_truth_masks = batch_inputs[0]["ground_truth_mask"].float() 
-      ground_truth_masks = ground_truth_masks.contiguous()[None, None, :, :]
-      ground_truth_masks = rearrange(ground_truth_masks, "b x w h -> b x h w").to(device)
+      ground_truth_masks = ground_truth_masks.contiguous()[None, :, :]
+      ground_truth_masks = rearrange(ground_truth_masks, "b w h -> b h w").to(device)
       print(gt_outputs_mask.shape, ground_truth_masks.shape)
       loss = seg_loss(gt_outputs_mask, ground_truth_masks)
 
