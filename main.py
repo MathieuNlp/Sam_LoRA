567c824cd2d6c3a42dc119f6d204de7e150e96cd
Mathieu <mathieu.nalpon@student-cs.fr>
2023-09-10 14:29:41 +0200

    colab update

--- main.py
+++ main.py
@@ -49,9 +49,9 @@ for epoch in range(num_epochs):
       ground_truth_masks = batch_inputs[0]["ground_truth_mask"].float().to(device)
       ground_truth_masks = ground_truth_masks.contiguous()[None, None, :, :]
       print(ground_truth_masks.shape)
-      ground_truth_masks = rearrange(ground_truth_masks, "b x w h -> b x h w")
+      ground_truth_masks = rearrange(ground_truth_masks, "b x w h -> b x h w", x=1)
       print(gt_outputs_mask.shape, ground_truth_masks.shape, ground_truth_masks[0].shape)
-      loss = seg_loss(gt_outputs_mask, ground_truth_masks.unsqueeze(1)[0])
+      loss = seg_loss(gt_outputs_mask, ground_truth_masks[0])
 
       # backward pass (compute gradients of parameters w.r.t. loss)
       optimizer.zero_grad()
