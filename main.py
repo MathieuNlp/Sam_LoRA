8261b652d369f3e22e61078678fb29d2e836a602
Mathieu <mathieu.nalpon@student-cs.fr>
2023-09-12 20:43:52 +0200

    colab

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ for epoch in range(num_epochs):
       optimizer.step()
       epoch_losses.append(loss.item())
       if epoch == 1 :
-         mask_predicted = outputs[0]["masks"]
+         mask_predicted = outputs[0]["masks"].squeeze(1)
          plt.plot(mask_predicted)
          plt.savefig("mask_pred.jpg")
          
