5d379ee42c19de57f49ca48d5b9272d636e6e69d
MathieuNlp <mathieunalpon@gmail.com>
2023-09-22 17:38:45 +0200

    training

--- src/segment_anything/build_sam.py
+++ src/segment_anything/build_sam.py
@@ -99,7 +99,7 @@ def _build_sam(
         pixel_mean=[123.675, 116.28, 103.53],
         pixel_std=[58.395, 57.12, 57.375],
     )
-    #sam.train()
+    sam.train()
     if checkpoint is not None:
         with open(checkpoint, "rb") as f:
             state_dict = torch.load(f)
