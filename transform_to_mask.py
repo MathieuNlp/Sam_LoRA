8b4e84a40fdfa44761e55459b05880f351d918aa
MathieuNlp <mathieunalpon@gmail.com>
2023-09-29 11:27:08 +0200

    new plots

--- transform_to_mask.py
+++ transform_to_mask.py
@@ -2,9 +2,9 @@ import PIL
 from PIL import Image
 import numpy as np
 
-filename = "ring4.jpg"
+filename = "ring1.jpg"
 mask_path = f"./dataset/mask_before_transform/{filename}"
 mask = Image.open(mask_path)
 gray = mask.convert('L')
-bw = gray.point(lambda x: 0 if x<1 else 255, '1')
+bw = gray.point(lambda x: 0 if x<10 else 255, '1')
 bw.save(f"./dataset/new_train/masks/{filename}")
